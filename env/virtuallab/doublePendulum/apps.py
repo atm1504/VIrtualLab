210ab9527ad012d79736fb46014b085bee2e4ed7
atm1504 <atm1504.in@gmail.com>
2021-03-26 17:21:19 +0530

    feat: Setting up django

---
+++ env/virtuallab/doublePendulum/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DoublependulumConfig(AppConfig):
+    name = 'doublePendulum'
---
+++ env/virtuallab/simplePendulum/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SimplependulumConfig(AppConfig):
+    name = 'simplePendulum'
