cc58872374f80a388e187cd0ec808a8220094682
atm1504 <atm1504.in@gmail.com>
2021-03-29 15:59:50 +0530

    Anime figured out

--- env/virtuallab/matplot/urls.py
+++ env/virtuallab/matplot/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import showplot
+from .views import showplot,showanimePlot
 
 
 urlpatterns = [
-    path("", showplot, name="showplot")
+    path("", showplot, name="showplot"),
+    path("anime/", showanimePlot, name="showanimeplot")
 ]
--- env/virtuallab/matplot/views.py
+++ env/virtuallab/matplot/views.py
@@ -4,6 +4,10 @@ import io
 import urllib
 import base64
 
+import numpy as np
+from matplotlib import animation
+
+
 # Create your views here.
 
 
@@ -18,3 +22,28 @@ def showplot(req):
     string = base64.b64encode(buf.read())
     uri = urllib.parse.quote(string)
     return render(req, template, {'data': uri})
+
+
+def showanimePlot(req):
+    # --------------------------Anime code
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+    line, = ax.plot([], [], lw=2)
+
+    def init():
+        line.set_data([], [])
+        return line,
+
+    def animate(i):
+        x = np.linspace(0, 2, 1000)
+        y = np.sin(2 * np.pi * (x - 0.01 * i))
+        line.set_data(x, y)
+        return line,
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=200, interval=20, blit=True)
+
+    print(type(anim))
+    plt.show()
+    template = "anime.html"
+    return render(req, template, {})
